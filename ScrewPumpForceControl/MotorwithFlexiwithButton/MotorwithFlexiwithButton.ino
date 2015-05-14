ae079ecd7670c3e9e592fc6cff6eee07b0e838e4
schaffer2013 <schaffer2013@gmail.com>
2015-05-14 14:38:11 -0400

    Button Mode Complete

--- ScrewPumpForceControl/MotorwithFlexiwithButton/MotorwithFlexiwithButton.ino
+++ ScrewPumpForceControl/MotorwithFlexiwithButton/MotorwithFlexiwithButton.ino
@@ -1,9 +1,13 @@
 int flexPin = A0;
+int modePin=7;
 int dirPin = 8;
 int pulsePin = 9;
 int resistorVal = 4700;
 int upperDBR = 6000;
 int lowerDBR = 2000;
+
+boolean buttonMode=false;
+boolean lastButtonPress=false;
 
 int delayMicros=45;
 
@@ -12,30 +16,41 @@ void setup() {
   Serial.begin(9600);
   pinMode(dirPin, OUTPUT);     //direction pin
   pinMode(pulsePin, OUTPUT); //step pin
+  pinMode(modePin, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   float flexVal = analogRead(flexPin);
   float flexVolt = flexVal * 5 / 1023;
   float flexR = resistorVal * (5 - flexVolt) / flexVolt;
+
+  boolean buttonPress=digitalRead(modePin);
+  if (buttonPress && !lastButtonPress && !buttonMode){
+    buttonMode=true;
+  }
+  else if (buttonPress && !lastButtonPress && buttonMode){
+    buttonMode=false;
+  }
+  lastButtonPress=buttonPress;
+  
   //Serial.println(flexR);
-  if (flexR > upperDBR) { //if less pressure, then turn motor CW
+  if (flexR > upperDBR && buttonMode) { //if less pressure, then turn motor CW
     digitalWrite(dirPin, HIGH);
     delayMicroseconds(delayMicros);
     Serial.println("CW");
     turnMotor();
   }
-  else if (flexR < lowerDBR) { //if more pressure, then turn motor CCW
+  else if (flexR < lowerDBR && buttonMode) { //if more pressure, then turn motor CCW
     digitalWrite(dirPin, LOW);
     delayMicroseconds(delayMicros);
     Serial.println("CCW");
     turnMotor();
-  } else {
+  } 
+  else {
     Serial.println("STABLE");
     delay(10);
   }
-  
+
 }
 
 void turnMotor() {
@@ -46,3 +61,6 @@ void turnMotor() {
     delayMicroseconds(delayMicros);
   }
 }
+
+
+
