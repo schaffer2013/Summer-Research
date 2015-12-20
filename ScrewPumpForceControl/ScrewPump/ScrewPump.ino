eca4a32f98cee91afecef0f4763e5f992969ed22
schaffer2013 <schaffer2013@gmail.com>
2015-12-20 11:05:05 -0500

    Commit

--- ScrewPumpForceControl/ScrewPump/ScrewPump.ino
+++ ScrewPumpForceControl/ScrewPump/ScrewPump.ino
@@ -1,28 +1,60 @@
 /*
 microstep driver ST-M5045
-Pul+ goes to +5V
-Pul- goes to Arduino Pin 9
-Dir+ goes to +5V
-Dir- goes to to Arduino Pin 8
-Enable+ to nothing
-Enable- to nothing
-*/
+ Pul+ goes to +5V
+ Pul- goes to Arduino Pin 9
+ Dir+ goes to +5V
+ Dir- goes to to Arduino Pin 8
+ Enable+ to nothing
+ Enable- to nothing
+ */
 
 int sensorPin = A3;
 int sensorValue = 0;
+int backwardPin = 2;
+int forwardPin=3;
+int limitPin=7;
+int currentStep=0
 
 void setup() {                
+  pinMode(backwardPin, INPUT);
+  pinMode(forwardPin, INPUT);
+  pinMode(limitPin, INPUT);
   pinMode(8, OUTPUT);     //direction pin
   pinMode(9, OUTPUT); //step pin
-  digitalWrite(8, LOW);
+  digitalWrite(8, HIGH);
   digitalWrite(9, LOW);
+  //attachInterrupt(1,kill,RISING) ;
 }
 
+//void swap(){
+//  if(digitalRead(8) == HIGH){
+//    digitalWrite(8, LOW);
+//  }
+//  else{
+//    digitalWrite(8, HIGH);
+//  }
+//}
+
 void loop() {
-  sensorValue = analogRead(sensorPin);
-  sensorValue = map(sensorValue,0,1023,3600,1);
-  digitalWrite(9, HIGH);
-  delayMicroseconds(sensorValue);          
-  digitalWrite(9, LOW); 
-  delayMicroseconds(sensorValue);
- }
+  if (digitalRead(backwardPin)){
+    digitalWrite(8, LOW);
+    digitalWrite(9, HIGH);
+    delayMicroseconds(500);          
+    digitalWrite(9, LOW); 
+    delayMicroseconds(500);
+  }
+  if (digitalRead(forwardPin)&!digitalRead(limitPin)){
+    digitalWrite(8, HIGH);
+    digitalWrite(9, HIGH);
+    delayMicroseconds(500);          
+    digitalWrite(9, LOW); 
+    delayMicroseconds(500);
+  }
+  //  if (!digitalRead(backwardPin)&!digitalRead(forwardPin)){
+  //    delay(10);
+  //  }
+}
+
+
+
+
